99f07f82f6debe00a705a0f19777cb269ebf0efe
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-29 21:39:06 -0300

    Routing y routing con paso de parametros

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def inicio():
     return 'Hola mundo! Los saludo cordialmente desde Flask'
+
+
+@app.route('/saludo/<nombre>')
+def saludo(nombre):
+    return f'Adios {nombre}! Me despido cordialmente desde Flask'
+
+@app.route('/edad/<int:edad>')
+def anios(edad):
+    return f'Hola! Tu edad es {edad} anios'
